635a40dd1091d2c7946683986f4a71a754f9ab0d
Farooq Ahmad <farooqahmad.hypersoft@gmail.com>
2023-12-20 16:28:07 +0500

    Initial Comment

---
+++ app/src/main/java/com/example/bottomappbardemo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.bottomappbardemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bottomappbardemo.databinding.ActivityMainBinding
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        // to set the rounded corner of the bottom app bar container
+        val bottomBarBackground = binding.bottomAppBar.background as MaterialShapeDrawable
+        bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel
+            .toBuilder()
+            .setTopRightCorner(CornerFamily.ROUNDED, 80f)
+            .setTopLeftCorner(CornerFamily.ROUNDED, 80f)
+            .setBottomRightCorner(CornerFamily.ROUNDED, 80f)
+            .setBottomLeftCorner(CornerFamily.ROUNDED, 80f)
+            .build()
+    }
+
+}
